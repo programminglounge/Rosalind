84fb17a50ad9289944a616c21c07fbd52a7d3d79
programminglounge <104110944+programminglounge@users.noreply.github.com>
2022-05-27 13:25:01 -0700

    Create MRNA.py

---
+++ MRNA.py
@@ -0,0 +1,16 @@
+RNA_REVERSE_TABLE = 
+{
+  'F':2, 'L':6, 'S':6, 'P':4, 'Y':2,
+  'I':3, 'V':4, 'A':4, 'T':4, 'G':4,
+  'R':6, 'K':2, 'D':2, 'N':2, 'E':2,
+  'Q':2, 'H':2, 'C':2, 'W':1, 'M':1
+  }
+  result = 3
+  stringRNA = input("Enter the RNA string:\n")
+  length = len(stringRNA)
+  if (length != 0):
+    for i in range (len(stringRNA)):
+      result = result * RNA_REVERSE_TABLE[stringRNA[i]]
+  else:
+    result = 0
+  print(result)
